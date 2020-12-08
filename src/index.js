1118d783880c3720b6eda6fe8e1840555417bc4e
waf503 <54567401+waf503@users.noreply.github.com>
2020-12-07 22:03:14 -0600

    Updates

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-//import HelloWorld from './components/HelloWorld'
-import App from './containers/App'
-
-ReactDOM.render(<App/>, document.getElementById('app'))
